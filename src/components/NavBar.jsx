da6e1252b4336d661f917ec54f359718ea3d9d1c
FIBA00 <fraolbulti0@gmail.com>
2026-08-06 14:11:41 +0300

    - new: added theme and base colors. - new: custom tailwind components. - new: added sidebar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
+import SideBar from "./SideBar";
 
 export default function Navbar() {
 	return (
 		<header className="h-16 bg-[#1A1E23]/80 backgdrop-blur-md border-b border-white/5 flex items-center justify-between px-8 shrink-0 ">
-			<h2 className="text-lg font-medium text-white">Hello there</h2>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/projects"> Projects</Link>
-				<Link to="/about">About</Link>
-			</nav>
-			<div className="flex items-center gap-4">
-				<div className="size-10 rounded-full metal-card glow-teal flex items-center justify-center text-xs text-teal-200">
-					Fraold
+			<SideBar />
+			<div>
+				<h2 className="text-lg font-medium text-white">Hello there</h2>
+				<nav className="flex-1 p-4 space-y-1 overflow-y-auto">
+					<Link to="/">Home</Link>
+					<Link to="/projects"> Projects</Link>
+					<Link to="/about">About</Link>
+				</nav>
+				<div className="flex items-center gap-4">
+					<div className="size-10 rounded-full metal-card glow-teal flex items-center justify-center text-xs text-teal-200">
+						Fraold
+					</div>
 				</div>
 			</div>
 		</header>
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+	return (
+		<aside className="w-64 bg-base border-r border-white flex flex-col h-full shrink-0">
+			<div className="h-16 flex items-center px-6 border-b border-white/5">
+				<span className="text-xl font-bold tracking-tight text-white flex items-center gap-2">
+					<span className="text-neon">F</span>
+					FIBA00
+					<span className="text-neon">Fullstack developer</span>
+				</span>
+			</div>
+		</aside>
+	);
+}
